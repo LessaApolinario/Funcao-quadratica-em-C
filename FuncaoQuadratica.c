d9a76a77c243b5c9f895329cf87eab6e68e24cd4
Lessa Apolinario <71906304+LessaApolinario@users.noreply.github.com>
2021-03-23 20:45:21 -0300

    Update FuncaoQuadratica.c

--- FuncaoQuadratica.c
+++ FuncaoQuadratica.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-/*Programa que verifica se a função quadrática possui zeros e os exibe em vídeo*/
+/*Programa que verifica se a funÃ§Ã£o quadrÃ¡tica possui zeros e os exibe em vÃ­deo*/
 int main (void) {
-	float a, b, c, x, y;//Coeficientes e variáveis da função.
-	float d, r1, r2;//Discriminante e raízes.
-	
-	y = a*pow(x,2) + b*x + c;//Lei de correspondência.
+	float a, b, c, x, y;//Coeficientes e variÃ¡veis da funÃ§Ã£o.
+	float d, r1, r2;//Discriminante e raÃ­zes.
 	
 	printf("Impute a, b e c: \n");
 	scanf("%f %f %f", &a, &b, &c);
 	
-	d = pow(b,2) - 4*a*c;//Discriminante da função quadrática("Delta").
+	y = a*pow(x,2) + b*x + c;//Lei de correspondÃªncia.
+	
+	d = pow(b,2) - 4*a*c;//Discriminante da funÃ§Ã£o quadrÃ¡tica("Delta").
 	
 	switch ( d > 0 ) {
 		case 1:
@@ -22,12 +22,12 @@ int main (void) {
 		case 0:
 			switch ( d < 0 ) {
 				case 1:
-					printf("Nao possui raizes reais!\n");//Discriminante negativo não gera raízes reais.
+					printf("Nao possui raizes reais!\n");//Discriminante negativo nÃ£o gera raÃ­zes reais.
 					break;
 				case 0:
 					switch ( d == 0 ) {
 						case 1:
-							r1 = r2 = -b / (2 * a);//Equivale a coordenada horizontal do vértice da parábola.
+							r1 = r2 = -b / (2 * a);//Equivale a coordenada horizontal do vÃ©rtice da parÃ¡bola.
 							printf("As raizes sao iguais e valem: %.2f\n", r1);//r1 = r2.
 							break;
 						case 0: printf("\aInvalido\n"); break;
